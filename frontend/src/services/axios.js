9d398b53f9d9a81fa45769837d186215ba7ad6fb
Nishank <156004996+Devnish0@users.noreply.github.com>
2026-05-01 13:46:27 +0530

    change: changed the backend url

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 const MODE = "production";
-let baseURL =
-  MODE === "production"
-    ? "https://sea-turtle-app-r54qb.ondigitalocean.app/"
-    : "/";
+let baseURL = MODE === "production" ? "https://intiger.onrender.com" : "/";
 const api = axios.create({
   baseURL: baseURL,
   withCredentials: true,
